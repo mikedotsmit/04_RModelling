24a339baf7040fa26caa445693a5aab5da49e70b
Michael C. Smit <mikedotsmit@gmail.com>
2020-03-31 12:30:56 +0700

    EMPLOY CENTRAL LIBRARIES PACKAGES
    - Employ easypackages to attach packages as vector. Recursive error write.table(packages) thus this step is excluded in this commit.
    - Centralise attaching packages needed for project with 'souce() from 'projectpackages.R'.
    - each Rmd source()s this projectspackages.R.
    Reduce issues with re-attach packes when indivivual Rmd is knitted during edits.
    - Improved table R2 headings to fit sidebyside tables.

---
+++ rpackages/projectpackages.R
@@ -0,0 +1,19 @@
+# packages
+# this is the central place where this project will attach packages from.
+# allpackeges are accumulated here (manualy).
+# 
+# the reason for doing this is to avoid duplicating all packages across all files.
+# this eases knitting from both the 'mother-' or base.Rmd and knitting from individual files as well.
+# add new packes in the list below names "mypackages" then
+# run the chunck code. the chunk will write a csv file that the other files contained within the prject folder will read and attach. 
+
+library(easypackages)
+library(here)
+
+mypackage <- c("rmd","patchwork","tidyverse","kableExtra","conflicted","tinytex","tidyverse","readxl","modelr","broom","reshape","readr","knitr","kableExtra","magrittr","qwraps2","xtable","gridExtra")
+
+libraries(mypackage) #attaches packages
+#packages(mypackage) #checks installed, installs, attaches.
+
+
+
