e9ebf39b874147c9181d71dcc6c322f50256cc03
Michael C. Smit <mikedotsmit@gmail.com>
2020-04-05 09:27:43 +0700

    new .rmd to generate kable table. i struggled to get the table save as immage. thus created a separate child .Rmd to knit the table (as pdf) image that can called from the mother base.RMD#. {r child = (file = here::here("03_code","tableR2.Rmd"))}

--- 02_rpackages/projectpackages.R
+++ 02_rpackages/projectpackages.R
@@ -10,10 +10,14 @@
 library(easypackages)
 library(here)
 
-mypackage <- c("rmd","patchwork","tidyverse","kableExtra","conflicted","tinytex","tidyverse","readxl","modelr","broom","reshape","readr","kableExtra","magrittr","qwraps2","xtable","gridExtra","svglite")
+mypackage <- c("magick", "rmd","patchwork","tidyverse","kableExtra","conflicted","tinytex","tidyverse","readxl","modelr","broom","reshape","readr","kableExtra","magrittr","qwraps2","xtable","gridExtra","svglite")
 
 libraries(mypackage) #attaches packages
 #packages(mypackage) #checks installed, installs, attaches.
 
 # exclude from packages vector as include in "rmd" families package: "knitr",
 
+### Software installed per Rstudio recommendation
+#Ghostscript ("for using magick package for LaTeX in HTML and Word) 
+#https://www.ghostscript.com/
+#HaoZhu's recommendation on page 25 of https://haozhu233.github.io/kableExtra/awesome_table_in_pdf.pdf
